5e0225411473b23ca7a01685963663601ed38330
Szeng89 <shijinz898@gmail.com>
2024-04-12 00:22:13 -0400

    made all currencies implement the convertable interface with 2 abstract methods

--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/AustralianDollar.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/AustralianDollar.java
@@ -1,6 +1,17 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class AustralianDollar implements ConvertableCurrency {
+
+    @Override
+    public Double convert(CurrencyType currencyType) {
+        return currencyType.getRate() / currencyType.AUSTRALIAN_DOLLAR.getRate();
+    }
+
+    @Override
+    public CurrencyType getName() {
+        return CurrencyType.AUSTRALIAN_DOLLAR;
+    }
 }
